d85170da94412efb06b04ec94460fa1705178740
sladuf <04fud27@gmail.com>
2022-07-20 14:50:59 +0900

    OPS-84 / feat: json parsing model & API

--- Eureka/Model/IngerdientModel.swift
+++ Eureka/Model/IngerdientModel.swift
@@ -1,38 +1,36 @@
-//
-//  IngerdientModel.swift
-//  Eureka
-//
-//  Created by 김민령 on 2022/07/13.
-//
 
 import Foundation
 import SwiftUI
 
 struct Ingredient: Codable {
-    let ingerdientId: Int
-    let ingredientName: String
-    let ingredientIcon: String
+    let id: Int
+    let name: String
+    let icon: String
 }
 
-//어떤 항목을 어떻게 표시할건지 아직 미정상태
-struct nutrientInfo: Codable {
-    let A: Int
-    let B: Int
-}
-
-struct userIngredient: Codable {
-    let userIngredientId: Int
-    let userIngredientInsertData: String
-    let userIngredientExpireData: String
+struct UserIngredient: Codable {
+    let userIngredientId: Int?
+    let insertData: String
+    let expireData: String
     let memo: String
     let ingredient: Ingredient
-    let nutrientDetail: nutrientInfo
+    let nutrientDetail: NutrientInfo?
 }
 
-struct customIngredient: Codable {
+struct CustomIngredient: Codable {
     let category: String?
     let ingredientName: String
     let enrollmentData: String
     let expireData: String
     let memo: String
 }
+
+
+//어떤 항목을 어떻게 표시할건지 아직 미정상태
+struct NutrientInfo: Codable {
+    let A: Int
+    let B: Int
+}
+
+
+
