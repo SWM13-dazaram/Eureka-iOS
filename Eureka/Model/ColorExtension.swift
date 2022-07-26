f00caf2dd315a49af0d63092f740a496dbb45cc2
sladuf <04fud27@gmail.com>
2022-07-26 20:56:18 +0900

    add: 아이콘 & 색상 추가

---
+++ Eureka/Model/ColorExtension.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+extension Color {
+    static let bg = Color("background")
+    static let appGreen = Color("appGreen")
+    static let title = Color("title")
+    static let appGray = Color("appGray")
+    static let shadow = Color("shadow")
+    static let appBlack = Color("appBlack")
+}
