cf3101607fc31a210f46a0e4f3fbcab5bcaf17a0
sladuf <04fud27@gmail.com>
2022-08-10 13:11:01 +0900

    [OPS-135] feat: 프로필 등록

---
+++ Eureka/View/Onboarding/SetProfileView.swift
@@ -0,0 +1,81 @@
+//
+//  SetProfileView.swift
+//  Eureka
+//
+//  Created by 김민령 on 2022/08/09.
+//
+
+import SwiftUI
+
+struct SetProfileView: View {
+    
+    @State var showImagePicker = false
+    @State var selectedUIImage: UIImage?
+    @State var image = Image("profileDefault")
+    @State var name = ""
+    
+    func loadImage() {
+        guard let selectedImage = selectedUIImage else { return }
+        image = Image(uiImage: selectedImage)
+    }
+    
+    var body: some View {
+        VStack{
+            HStack{
+                MainTitle("프로필을 등록해주세요 :)")
+                    .frame(width: 160)
+                Spacer()
+            }
+            ZStack{
+                //@TODO: 크기 조절하기
+                image
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(Circle())
+                Button {
+                    showImagePicker.toggle()
+                } label: {
+                    Image("photo")
+                }
+                .sheet(isPresented: $showImagePicker, onDismiss: {
+                    loadImage()
+                }) {
+                    ImagePicker(image: $selectedUIImage)
+                }
+                .offset(x: 40, y: 40)
+            }
+            .padding()
+            HStack{
+                Text("닉네임")
+                    .foregroundColor(.appBlack)
+                    .font(.system(size: 13, weight: .bold))
+                Spacer()
+            }
+            ZStack{
+                TextField("사용하실 닉네임을 입력해주세요.", text: $name)
+                    .font(.system(size: 14))
+                    .padding()
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke()
+                    .frame(height: 45)
+                    .foregroundColor(name=="" ? .barBackground : .appBlack)
+            }
+            Spacer()
+            // @TODO: 중복체크 기능 넣은 후 코드 변경
+            if name != "" {
+                Button {
+                    
+                } label: {
+                    BottomButton(text: "완료")
+                }
+            }
+        }
+        .padding(.init(top: 30, leading: 30, bottom: 0, trailing: 30))
+    }
+}
+
+struct SetProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetProfileView()
+    }
+}
---
+++ Eureka/ViewModel/ImagePicker.swift
@@ -0,0 +1,45 @@
+//
+//  ImagePicker.swift
+//  Eureka
+//
+//  Created by 김민령 on 2022/08/09.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var mode
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: Context) -> some UIViewController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+extension ImagePicker {
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let image = info[.originalImage] as? UIImage else { return }
+            parent.image = image
+            parent.mode.wrappedValue.dismiss()
+        }
+    }
+}
